bf5cff9cef734f7b8cfed410a269261432471095
Daniel Bady <badydaniel@seznam.cz>
2023-12-04 18:48:20 +0100

    Ugly

--- index.ts
+++ index.ts
@@ -6,5 +6,4 @@ export default {
   name: "GogoAnime",
   description: "basic GogoAnime module \nThank Consumet for their extractors:\n https://github.com/consumet/consumet.ts",
   author: "DeNeRr",
-  icon: "https://github.com/danielbady/Gogo-module/blob/main/assets/icon.png"
 } satisfies RepoMetadata
